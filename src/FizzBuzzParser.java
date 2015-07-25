2c4d536c7a8dff328b55676e41d141235e9851e9
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-25 12:54:35 +0100

    Refactor: general

--- src/FizzBuzzParser.java
+++ src/FizzBuzzParser.java
@@ -2,15 +2,28 @@ public class FizzBuzzParser {
     public String parse(int length) {
         String parsedSecuence = "";
         for (int i = 1; i <= length; i++) {
-            parsedSecuence += parseNumber(i);
+            parsedSecuence += parseNumber(i) + "\n";
         }
         return parsedSecuence;
     }
 
     private String parseNumber(int number) {
-        if (number % 2 == 0 && number % 3 == 0) return "FizzBuzz\n";
-        if (number % 2 == 0) return "Fizz\n";
-        if (number % 3 == 0) return "Buzz\n";
-        return number + "\n";
+        String parsedNumber = parseIfDivisibleByTwo(number) + parseIfDivisibleByThree(number);
+        return parsedNumber + defaultParseIfEmpty(parsedNumber, number);
+    }
+
+    private String defaultParseIfEmpty(String parsedNumber, int number){
+        if (parsedNumber.equals("")) return String.valueOf(number);
+        return "";
+    }
+
+    private String parseIfDivisibleByTwo(int number){
+        if (number % 2 == 0) return "Fizz";
+        return "";
+    }
+
+    private String parseIfDivisibleByThree(int number){
+        if (number % 3 == 0) return "Buzz";
+        return "";
     }
 }
