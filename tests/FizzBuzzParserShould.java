47aa3e4a0dd27b7dbee897099d8d106e28b5d724
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-25 12:55:04 +0100

    Remove to do list

--- tests/FizzBuzzParserShould.java
+++ tests/FizzBuzzParserShould.java
@@ -5,14 +5,6 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzBuzzParserShould {
 
-    /**
-     * TODO: para un número devuelve una secuencia desde el número 1 hasta el input
-     * TODO: la secuencia debe estar parseada por las siguientes reglas
-     *  cuando un número es divisible por 2 y 3 remplazamos por FizzBuzz
-     *
-     *  Examples:
-     *   6 => 1\Fizz\Buzz\Fizz\5\FizzBuzz
-     */
     @Test
     public void not_parse_numbers_when_they_are_not_divisible_by_two_or_three() {
         assertThat(new FizzBuzzParser().parse(1), is("1\n"));
